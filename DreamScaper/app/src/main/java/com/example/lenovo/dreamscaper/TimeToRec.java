d99c9e1d3660dcebf8a27081f6ad8657f7eafaea
SaatvikMahajan <40631766+SaatvikMahajan@users.noreply.github.com>
2019-11-26 17:37:48 +0530

    activities update

---
+++ DreamScaper/app/src/main/java/com/example/lenovo/dreamscaper/TimeToRec.java
@@ -0,0 +1,14 @@
+package com.example.lenovo.dreamscaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeToRec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_to_rec);
+    }
+}
